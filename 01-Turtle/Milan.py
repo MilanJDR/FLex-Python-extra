7a3e4f911510571846f0341d9cb534e7215661f7
Milan de Ridder <30842@ma-web.nl>
2020-09-24 15:04:30 +0200

    Python bestand aangepast

--- 01-Turtle/Milan.py
+++ 01-Turtle/Milan.py
@@ -12,4 +12,15 @@ for x in range(4):
     turtle.forward(100)
     turtle.right(90)
 
+for x in range(36):
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.left(5)
+
+turtle.end_fill()
 turtle.done()
