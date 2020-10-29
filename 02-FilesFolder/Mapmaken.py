87d240e5e79455dfb006be20b6744c480763a0b1
Milan de Ridder <30842@ma-web.nl>
2020-10-29 14:08:16 +0100

    Mapmaken toegevoegd

---
+++ 02-FilesFolder/Mapmaken.py
@@ -0,0 +1,11 @@
+import os
+
+mapnaam = input("Welke naam wil je voor de map? ")
+
+
+lengte_mapnaam = len(mapnaam)
+if lengte_mapnaam > 0:
+    os.mkdir(mapnaam)
+    print("De map " + mapnaam + " is gemaakt.")
+else:
+    print("Je hebt geen naam ingevoerd")
